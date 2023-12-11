452d0f6f357110bdaf5cc237c2b036fcb6689981
Your Name <you@example.com>
2023-12-11 15:22:12 +0100

    created addToShoppingCart()

--- script.js
+++ script.js
@@ -2,33 +2,38 @@
 let dishes = [
     {
         'title': 'dish1',
-        'description': 'Rindsuppe',
-        'option': 'Fritatten, Backerbsen',
+        'ingredients': 'Rindsuppe',
+        'options': 'Fritatten, Backerbsen',
         'price': 3.50,
+        'in-cart': false
     },
     {
         'title': 'Risipisi',
-        'description': 'Reis, Erbsen und Huhn',
-        'option': 'Mais, Speckwürfel',
+        'ingredients': 'Reis, Erbsen und Huhn',
+        'options': 'Mais, Speckwürfel',
         'price': 6.00,
+        'in-cart': false
     },
     {
         'title': 'dish3',
-        'description': 'Lasagne',
-        'option': '',
+        'ingredients': 'Lasagne',
+        'options': '',
         'price': 8.00,
+        'in-cart': false
     },
     {
         'title': 'dish4',
-        'description': 'Gebackene Calamaris',
-        'option': '1,00 € pro zusätzlichem Stück',
+        'ingredients': 'Gebackene Calamaris',
+        'options': '1,00 € pro zusätzlichem Stück',
         'price': 12.00,
+        'in-cart': false
     },
     {
         'title': 'dish5',
-        'description': 'Apfelkuchen',
-        'option': 'Zimt',
+        'ingredients': 'Apfelkuchen',
+        'options': 'Zimt',
         'price': 5.50,
+        'in-cart': false
     }
 ];
 
@@ -36,24 +41,147 @@ let shoppingCart = [];
 
 
 // Functions
+load();
+
+
+function showDishes() {    // Bitte bearbeiten!!!
+    let restaurantDishes = document.getElementById('restaurant-dishes');
+    restaurantDishes.innerHTML = '';
+    for (let i = 0; i < dishes.length; i++) {
+        restaurantDishes.innerHTML += `
+        <div id="dish-card-${i}" class="dish-card">
+            <div class="dish-card-top">
+                <h4 class="dish-card-title">${getTitle(i)}</h4>
+                <button id="menu-button" class="button" onclick="addDish(${i})">+</button>
+            </div>
+            <div class="dish-card-description">
+                <p class="dish-card-ingredients">
+                    Reis, Erbsen und Huhn
+                </p>
+                <p class="dish-card-option">
+                    Auch mit Mais und Speckwürfeln.
+                </p>
+                <div class="dish-card-price">${getPrice(i)}</div>
+            </div>
+        </div>
+    `;
+    }
+}
+
+
 function addDish(i) {
-    let title = getTitle(i);
-    let price = getPrice(i);
-    addToShoppingCart(title, price);
+    addOrIncreaseIf(i);
+    saveAndShowDishes();
+}
+// if shopppingCart == false, add dish, else increase amount
+
+
+function addOrIncreaseIf(i) {
+    if (alreadyInCart(i)) {
+        increaseAmount(i);
+    } else {
+        addToShoppingCart(i);
+        setInCart(i);
+    }
+}
+
+
+function alreadyInCart(i) {
+    return dishes[i]['in-cart'];
+}
+
+
+function increaseAmount(i) {
+    let requestedIndex = 0;
+    getRequestedIndex(requestedIndex, i);
+    setAmount(requestedIndex);
+}
+
+
+function getRequestedIndex(requestedIndex, i) {
+    while (shoppingCart[requestedIndex]['title'] != getTitle(i)) {
+        requestedIndex++;
+    }
+}
+
+
+function setAmount(i) {
+    shoppingCart[i]['amount']++;
+}
+
+
+function increaseAmount(i) {
+    shoppingCart[i]['amount']++;
+}
+
+
+function addToShoppingCart(i) {
+    let nextIndex = getNextIndex();
+    shoppingCart[nextIndex] = {
+        'title': getTitle(i),
+        'price': getPrice(i),
+        'amount': 1
+    }
+}
+
+
+function getNextIndex() {
+    return shoppingCart.length;
 }
 
 
 function getTitle(i) {
-    return dishCards[i]['title'];
+    return dishes[i]['title'];
 }
 
 
 function getPrice(i) {
-    return dishCards[i]['price'];
+    return dishes[i]['price'];
 }
 
 
-function addToShoppingCart(title, price) {
-    shoppingCart['title'].push(title);
-    shoppingCart['price'].push(price);
+function setInCart(i) {
+    dishes[i]['in-cart'] = true;
+}
+
+
+function save() {
+    let keys = ['dishes', 'shoppingCart'];
+    let variables = [dishes, shoppingCart];
+    stringifyAndSetItem(keys, variables);
+}
+
+
+function stringifyAndSetItem(keys, variables) {
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i];
+        let variable = variables[i];
+        let variableAsText = JSON.stringify(variable);
+        localStorage.setItem(key, variableAsText);
+    }
+}
+
+
+function saveAndShowDishes() {
+    save();
+    showDishes();
+}
+
+
+function load() {
+    let keys = ['dishes', 'shoppingCart'];
+    let variables = [dishes, shoppingCart];
+    getItemAndParse(keys, variables);
+}
+
+
+function getItemAndParse(keys, variables) {
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i];
+        let variable = variables[i];
+        let variableAsText = localStorage.getItem(key);
+        if (variableAsText) {
+            variable = JSON.parse(variableAsText);
+        }
+    }
 }
