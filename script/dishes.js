83a24e2863a36d046287e19ff95362108aa361c2
Your Name <you@example.com>
2023-12-15 16:51:08 +0100

    enhanced functions til decimal prices

--- script/dishes.js
+++ script/dishes.js
@@ -1,66 +1,104 @@
 // Variables
 let dishes = [
-    {    // dish with index 0
-        'title': 'dish1',    // title of dish
-        'ingredients': 'Rindsuppe',    // ingredients of dish
-        'option': 'Fritatten, Backerbsen',    // optional ingredients of dish
-        'price': 3.50,    // price of dish
-        'in-cart': false    // true, if in shopping cart
+    {
+        'title': 'Mexicana scharf',
+        'description': 'Paradeiser, Käse, Hühnerfleisch, Paprika, Mais, Chilli',
+        'price': 9.50,
+        'option': 'große Pizza',
+        'option-price': 2.40,
+        'in-cart': false
     },
     {
-        'title': 'Risipisi',
-        'ingredients': 'Reis, Erbsen und Huhn',
-        'option': 'Mais, Speckwürfel',
+        'title': 'Frutti di Mare',
+        'description': 'Paradeiser, Käse, Meeresfrüchte, Knoblauch',
+        'price': 9.90,
+        'option': 'große Pizza',
+        'option-price': 2.60,
+        'in-cart': false
+    },
+    {
+        'title': 'Pizzastangerl',
+        'description': '2 Stück',
+        'price': 1.50,
+        'option': 'plus 3 Stück',
+        'option-price': 1.50,
+        'in-cart': false
+    },
+    {
+        'title': 'Spaghetti Bolognese',
+        'description': 'Fleischsauce',
+        'price': 9.90,
+        'option': 'keine',
+        'option-price': 0,
+        'in-cart': false
+    },
+    {
+        'title': 'Lasagne',
+        'description': 'keine',
+        'price': 9.90,
+        'option': 'keine',
+        'option-price': 0,
+        'in-cart': false
+    },
+    {
+        'title': 'Frühlingsrollen',
+        'description': 'mit süß-saurer Sauce, 5 Stück',
+        'price': 5.90,
+        'option': 'plus 3 Stück',
+        'option-price': 2.40,
+        'in-cart': false
+    },
+    {
+        'title': 'Paella Reis',
+        'description': 'Curry Reis, Shrimps, Frutti di Mare, Hühnerfleisch und Salat',
+        'price': 10.50,
+        'option': 'große Portion',
+        'option-price': 1.40,
+        'in-cart': false
+    },
+    {
+        'title': 'Nusspalatschinken',
+        'description': '2 Stück',
         'price': 6.00,
-        'in-cart': false
-    },
-    {
-        'title': 'dish3',
-        'ingredients': 'Lasagne',
-        'option': '',
-        'price': 8.00,
-        'in-cart': false
-    },
-    {
-        'title': 'dish4',
-        'ingredients': 'Gebackene Calamaris',
-        'option': '1,00 € pro zusätzlichem Stück',
-        'price': 12.00,
-        'in-cart': false
-    },
-    {
-        'title': 'dish5',
-        'ingredients': 'Apfelkuchen',
-        'option': 'Zimt',
-        'price': 5.50,
+        'option': 'plus 1 Stück',
+        'option-price': 2.00,
         'in-cart': false
     }
-];    // contains all available dishes
+];
 
 
 // Functions
-load();
+loadDishes();
+
+
+function render() {
+    showDishes();
+    showItems();
+}
 
 
 function showDishes() {    // shows all available dishes of restaurant
     let dishCardCollector = document.getElementById('dish-card-collector');    // contains the element 'dish-card-collector'
     dishCardCollector.innerHTML = '';    // empties dishCardCollector
     fillDishCardCollector(dishCardCollector);
-
-    showShoppingCartItems();    // Bitte bearbeiten!!!
-    setValuesInCart();    // Bitte bearbeiten!!!
+    save();
 }
 
 
 function fillDishCardCollector(dishCardCollector) {    // fills dishCardCollector with dish cards
     for (let i = 0; i < dishes.length; i++) {
-        dishCardCollector.innerHTML += `
+        dishCardCollector.innerHTML += writeDishCard(i);
+    }
+}
+
+
+function writeDishCard(i) {
+    return `
         <article id="dish-card-${i}" class="dish-card">
             ${writeHeader(i)}
             ${writeDescription(i)}
         </article>
     `;    // writes the dish card i
-    }
 }
 
 
@@ -68,7 +106,7 @@ function writeHeader(i) {    // writes the header of dish card i
     return `
         <div id="dish-card-header-${i}" class="display-between-center">
             <h3 id="dish-card-title-${i}" class="dish-card-title">${getTitle(i)}</h3>
-            <button id="add-dish-button-${i}" class="button" onclick="addDish(${i})">+</button>
+            <button id="add-dish-button-${i}" class="button dish-card-button" onclick="addDish(${i})">+</button>
         </div>
     `;
 }
@@ -81,17 +119,17 @@ function getTitle(i) {    // provides the title of dish card i
 
 function writeDescription(i) {    // writes the description of dish card i
     return `
-        <div id="dish-card-description-${i}" class="dish-card-description column-start-start">
-            <p id="dish-card-ingredients-${i}" class="dish-card-ingredients">${getIngredients(i)}</p>
-            <p id="dish-card-option-${i}" class="dish-card-option">${getOption(i)}</p>
-            <div id="dish-card-price-${i}" class="dish-card-price">${getPrice(i)}</div>
+        <div id="dish-card-description-${i}" class="column-start-start">
+            <p id="dish-card-ingredients-${i}" class="dish-card-ingredients">${getDescription(i)}</p>
+            <p id="dish-card-option-${i}" class="dish-card-option">Option: ${getOption(i)}</p>
+            <div id="dish-card-price-${i}" class="dish-card-price">${getDecimalPrice(i)} €</div>
         </div>
     `;
 }
 
 
-function getIngredients(i) {    // provides the ingredients of dish card i
-    return dishes[i]['ingredients'];
+function getDescription(i) {    // provides the description of dish card i
+    return dishes[i]['description'];
 }
 
 
@@ -100,6 +138,38 @@ function getOption(i) {    // provides the option of dish card i
 }
 
 
+function getDecimalPrice(i) {
+    let price = getPrice(i);
+    let roundedPrice = Math.round(price * 100) / 100;
+    return roundedPrice.toFixed(2);
+}
+
+
 function getPrice(i) {    // provides the price of dish card i
     return dishes[i]['price'];
+}
+
+
+function save() {    // saves global variables to the local storage
+    let keys = ['dishes', 'shoppingCart'];    // contains global variables' keys
+    let variables = [dishes, shoppingCart];    // contains global variables
+    stringifyAndSetItem(keys, variables);
+}
+
+
+function stringifyAndSetItem(keys, variables) {    // creates Strings and sets items at the local storage
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i];    // contains the key i
+        let variable = variables[i];    // contains the variable i
+        let variableAsText = JSON.stringify(variable);    // creates a String of variable i
+        localStorage.setItem(key, variableAsText);    // sets this item at the local storage
+    }
+}
+
+
+function loadDishes() {
+    let dishesAsText = localStorage.getItem('dishes');
+    if (dishesAsText) {
+        dishes = JSON.parse(dishesAsText);
+    }
 }
--- script/shopping-cart.js
+++ script/shopping-cart.js
@@ -3,9 +3,130 @@ let shoppingCart = [];    // contains all selected dishes
 
 
 // Functions
+loadShoppingCart();
+
+
+function showItems() {
+    let itemCollector = document.getElementById('shopping-cart-item-collector');
+    itemCollector.innerHTML = '';
+    fillItemCollector(itemCollector);
+    save();
+}
+
+
+function fillItemCollector(itemCollector) {
+    for (let i = 0; i < shoppingCart.length; i++) {
+        itemCollector.innerHTML += writeItem(i);
+    }
+}
+
+
+function writeItem(i) {
+    return `
+    <div id="shopping-cart-item-${i}" class="shopping-cart-item">
+        <table>
+            ${writeTableTr(i)}
+        </table>
+    </div>
+`;
+}
+
+
+function writeTableTr(i) {
+    return `
+        <tr>
+            <td id="item-index-${i}" class="item-index fw-700">${i}</td>
+            <td class="column-start-start item-details">
+                ${writeTitleAndPrice(i)}
+                ${writeOption(i)}
+                ${writeNotesAndAmount(i)}
+            </td>
+        </tr>
+    `;
+}
+
+
+function writeTitleAndPrice(i) {
+    return `
+        <div id="item-title-and-price-${i}" class="width-100 display-between-center gap-20">
+            <div id="item-title-${i}" class="added-item-title fw-700">${getTitleOfDish(i)}</div>
+            <div id="item-price-${i}" class="item-price fw-700">${getDecimalPriceInCart(i)} €</div>
+        </div>
+    `;
+}
+
+
+function getTitleOfDish(i) {
+    let index = getDishId(i);
+    return dishes[index]['title'];
+}
+
+
+function getDishId(i) {
+    return shoppingCart[i]['dish-id'];
+}
+
+
+function getDecimalPriceInCart(i) {
+    let price = getPriceInCart(i);
+    let roundedPrice = Math.round(price * 100) / 100;
+    return roundedPrice.toFixed(2);
+}
+
+
+function getPriceInCart(i) {
+    return shoppingCart[i]['price'];
+}
+
+
+function writeOption(i) {
+    return `
+        <div id="added-option-${i}" class="added-options">${getOptionInCart(i)}</div>
+    `;
+}
+
+
+function getOptionInCart(i) {
+    return shoppingCart[i]['option'];
+}
+
+
+function writeNotesAndAmount(i) {
+    return `
+        <div class="mt-8 display-between-center gap-20">
+            <div class="width-50 item-notes">Anmerkungen hinzufügen</div>
+            <div class="width-50 display-between-center">
+                <button id="menu-plus-button-${i}" class="button" onclick="increaseItems(${i})">+</button>
+                <div id="item-amount-${i}" class="item-amount">${getAmountInCart(i)}</div>
+                <button id="menu-minus-button-${i}" class="button" onclick="decreaseItems(${i})">-</button>
+            </div>
+        </div>
+    `;
+}
+
+
+function getAmountInCart(i) {
+    return shoppingCart[i]['amount'];
+}
+
+
+function loadShoppingCart() {
+    let shopingCartAsText = localStorage.getItem('shoppingCart');
+    if (shopingCartAsText) {
+        shoppingCart = JSON.parse(shopingCartAsText);
+    }
+}
+
+
 function addDish(i) {    // adds dish i to the shopping cart
     addOrIncreaseIf(i);
-    saveAndShowDishes();
+    saveAndShowItems();
+}
+
+
+function saveAndShowItems() {
+    save();
+    showItems();
 }
 
 
@@ -14,7 +135,6 @@ function addOrIncreaseIf(i) {
         updateAmountAndPrice(i);    // update amount and price of item i
     } else {    // else ...
         addToShoppingCart(i);    // add item to the shopping cart
-        setInCart(i);    // set dishes[i]['in-cart'] = true
     }
 }
 
@@ -23,3 +143,99 @@ function alreadyInCart(i) {    // returns true or false
     return dishes[i]['in-cart'];
 }
 
+
+function updateAmountAndPrice(i) {    // sets the current amount and price of the requested item
+    let itemId = getItemId(i);    // contains the index of the requested item in the shopping cart
+    increaseAmountInCart(itemId);
+    increasePriceInCart(i, itemId);
+}
+
+
+function getItemId(i) {
+    return dishes[i]['item-id'];
+}
+
+
+function increaseAmountInCart(itemId) {
+    shoppingCart[itemId]['amount']++;
+}
+
+
+function increasePriceInCart(i, itemId) {
+    let price = getPrice(i);
+    shoppingCart[itemId]['price'] += price;
+}
+
+
+function addToShoppingCart(i) {    // adds the dish i to shopping cart
+    let currentIndex = getCurrentIndex();    // contains the index of the current item
+    shoppingCart[currentIndex] = {
+        'dish-id': i,    // contains the id of dish i
+        'item-id': currentIndex,    // contains the id of the new item
+        'price': getPrice(i),    // takes the price of dish i
+        'option': 'keine / nicht ausgewählt',    // takes the option of dish i
+        'amount': 1,    // sets amount = 1
+    };    // adds the dish i to shopping cart
+
+    dishes[i]['in-cart'] = true,
+    dishes[i]['item-id'] = currentIndex;
+}
+
+
+function getCurrentIndex() {    // provides the current index for the new item
+    return shoppingCart.length;
+}
+
+
+function increaseItems(i) {
+    increaseAmount(i);
+    increasePrice(i);
+    saveAndShowItems();
+}
+
+
+function increaseAmount(i) {
+    shoppingCart[i]['amount']++;
+}
+
+
+function increasePrice(i) {
+    let priceInCart = getPriceInCart(i);
+    let dishId = getDishId(i);
+    let price = getPrice(dishId);
+    let currentPrice = priceInCart + price;
+    shoppingCart[i]['price'] = currentPrice;
+}
+
+
+function decreaseItems(i) {
+    decreaseAmount(i);
+    saveAndShowItems();
+}
+
+
+function decreaseAmount(i) {
+    let amount = getAmountInCart(i);
+    decreaseOrDeleteItem(amount, i);
+}
+
+
+function decreaseOrDeleteItem(amount, i) {
+    if (amount > 1) {
+        shoppingCart[i]['amount']--;
+        decreasePrice(i);
+    } else {
+        let dishId = getDishId(i);
+        dishes[dishId]['in-cart'] = false;
+        shoppingCart.splice(i, 1);
+    }
+}
+
+
+function decreasePrice(i) {
+    let priceInCart = getPriceInCart(i);
+    let dishId = getDishId(i);
+    let price = getPrice(dishId);
+    let currentPrice = priceInCart - price;
+    shoppingCart[i]['price'] = currentPrice;
+}
