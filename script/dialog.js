a0d4b87d1b4be1739509acd931cdde51910a36b3
Your Name <you@example.com>
2023-12-16 13:32:30 +0100

    prepared current price total of dialog

--- script/dialog.js
+++ script/dialog.js
@@ -59,7 +59,7 @@ function writeDialogBoxDescripton(i) {
     return `
         <div class="dialog-box-description column-start-start">
             <p class="dialog-box-ingredients">${getDescription(i)}</p>
-            <span class="dialog-box-price">${getDecimalPrice(i)} €</span>
+            <span class="dialog-box-price"><output id="dialog-box-price">${getDecimalPrice(i)}</output> €</span>
         </div>
     `;
 }
@@ -70,18 +70,24 @@ function writeDialogBoxOption(i) {
         <div class="dialog-box-option">
             <div class="dialog-box-option-text">Ihre Option:</div>
             <div class="dialog-box-option-group display-between-center">
-                <span>${getOptionAndOptionPrice(i)}</span>
-                <button id="option-button" class="option-button">Auswählen</button>
+                <span>${getOption(i)} (<output id="dialog-box-option-price">${getDecimalOptionPrice(i)}</output> €)</span>
+                <button id="option-button" class="option-button" onclick="${setOptionSelected(i)}">Auswählen</button>
             </div>
         </div>
     `;
 }
 
 
-function getOptionAndOptionPrice(i) {
-    let option = getOption(i);
-    let optionPrice = getOptionPrice(i).toFixed(2);
-    return `${option} (+ ${optionPrice} €)`;
+// function getOptionAndOptionPrice(i) {
+//     let option = getOption(i);
+//     let optionPrice = getOptionPrice(i).toFixed(2);
+//     return `${option} (+ ${optionPrice} €)`;
+// }
+
+
+function getDecimalOptionPrice(i) {
+    let optionPrice = getOptionPrice(i);
+    return optionPrice.toFixed(2);
 }
 
 
@@ -90,16 +96,31 @@ function getOptionPrice(i) {
 }
 
 
+function setOptionSelected(i) {
+    let optionSelected = getOptionSelected(i);
+    if (optionSelected) {
+        dishes[i]['option-selected'] = false;
+    } else {
+        dishes[i]['option-selected'] = true;
+    }
+}
+
+
+function getOptionSelected(i) {
+    return dishes[i]['option-selected'];
+}
+
+
 function writeDialogBoxFooter(i) {
     return `
         <div class="dialog-box-footer display-start-center">
             <div class="dialog-box-amount-group display-between-center">
                 <button id="dialog-box-plus-button" class="button">+</button>
-                <span id="dialog-box-item-amount" class="item-amount">${getAmountInCartIf(i)}</span>
+                <span id="dialog-box-item-amount" class="item-amount"><output id="dialog-box-amount">${getAmountInCartIf(i)}</output></span>
                 <button id="dialog-box-minus-button" class="button">-</button>
             </div>
             <button id="dialog-box-add-button" class="dialog-box-add-button" onclick="confirmAction(${i})">
-                <span><output>${calculateTotalDialog(i)}</output> €</span>
+                <span><output id="dialog-box-total-price">${calculateTotalDialog(i)}</output> €</span>
             </button>
         </div>
     `;
@@ -109,7 +130,7 @@ function writeDialogBoxFooter(i) {
 function getAmountInCartIf(i) {
     let inCart = getInCart(i);
     if (inCart) {
-        let itemId = getItemId(i);
+        let itemId = getItemId(i);    // in Gebrauch?
         return shoppingCart[itemId]['amount'];
     } else {
         return 1;
@@ -130,8 +151,18 @@ function confirmAction(i) {
 
 function calculateTotalDialog(i) {
     let price = getPrice(i);
-    let optionPrice = getOptionPrice(i);
+    let optionPrice = getOptionPriceIf(i);
     let totalPrice = price + optionPrice;
     let amount = getAmountInCartIf(i);
     return (amount * totalPrice).toFixed(2);
+}
+
+
+function getOptionPriceIf(i) {
+    let optionSelected = getOptionSelected(i);
+    if (optionSelected) {
+        return getOptionPrice(i);
+    } else {
+        return 0;
+    }
 }
